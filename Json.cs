16819d27f90f9ad3cd412378324e45ba55bfa1db
ssrmm <ssrmm@users.noreply.github.com>
2022-05-05 17:27:11 +0200

    Create MWE

---
+++ Json.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json.Serialization;
+
+namespace WpfApp;
+
+public class Foo
+{
+    int Bar { get; set; }
+}
+
+[JsonSerializable(typeof(Foo))]
+public partial class FooSerializerContext : JsonSerializerContext
+{
+}
